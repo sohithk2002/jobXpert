1326956b5ba02004dd8e6f68e88199b20363135e
sohith kampalli <sohithkampalli@gmail.com>
2025-04-20 18:58:16 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,24 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              connect-src 'self' https://clerk-telemetry.com https://scdn.clerk.com https://api.stripe.com https://*.js.stripe.com;
-              script-src 'self' 'unsafe-inline' https://*.js.stripe.com;
-              frame-src https://js.stripe.com;
-            `.replace(/\s{2,}/g, " ").trim(),
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
